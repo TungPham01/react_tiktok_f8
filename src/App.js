d4369d037de35a84dfe8cff98dceac01a0a403c0
Tung Pham <phamduytung11@gmail.com>
2022-09-19 11:13:10 +0700

    checkbox form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const courses = [
+  {
+    id: 1,
+    name: "PHP",
+  },
+  {
+    id: 2,
+    name: "JAVA",
+  },
+  {
+    id: 3,
+    name: "Python",
+  },
+];
 function App() {
-  const [counter, setCounter] = useState(1);
-  const handleIncrease = () => {
-    setCounter(counter + 1)
+  const [checked, setChecked] = useState([]);
+
+  console.log(checked);
+
+  const handleChecked = (id) => {
+    if (checked.includes(id)) {
+      setChecked(checked.filter((item) => {
+        return item !== id;
+      }));
+    } else {
+      setChecked([...checked, id]);
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log({ids: checked})
   }
+
   return (
-    <div className="App">
-        <h1>{counter}</h1>
-        <button onClick={handleIncrease}>Increase</button>
+    <div style={{ padding: 32 }}>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <input
+            type="checkbox"
+            checked={checked.includes(course.id)}
+            onChange={() => handleChecked(course.id)}
+          />
+          {course.name}
+        </div>
+      ))}
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
