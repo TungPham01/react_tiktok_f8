9b05b582fe13abb0418b6c96571dda0ed29da0f4
Tung Pham <phamduytung11@gmail.com>
2022-09-20 17:33:24 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Content from "./Content";
 
 function App() {
 
-  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(60);
+  let timerId = useRef()
 
+  const handleStart = () => {
+    timerId.current = setInterval(() => (
+      setCount(prev => prev - 1)
+    ), 1000)
+  }
+
+  const handleStop = () => {
+    clearInterval(timerId.current)
+  }
+
+  const h1Ref = useRef()
+  console.log(h1Ref.current)
   return (
     <div style={{ padding: 32 }}>
-      <button onClick={() => setShow(!show)}>Toggle</button>
-      { show &&
-        <Content></Content>
-      }
+      <h1 ref={h1Ref}>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 }
